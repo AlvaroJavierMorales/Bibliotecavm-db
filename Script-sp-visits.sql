328b195833909f0c0b6e3002044722e2e0cd23f8
Alvaro Morales <moralestombe19@gmail.com>
2025-05-03 21:39:38 -0500

    se ajusta el sp actualizarduracionvisita

--- Script-sp-visits.sql
+++ Script-sp-visits.sql
@@ -19,6 +19,7 @@ BEGIN
         v_usu_id,
         v_mat_id
     ); 
+     SELECT LAST_INSERT_ID() AS vis_id;
 END //
 DELIMITER ;
 
@@ -222,11 +223,12 @@ DELIMITER ;
 DELIMITER //
 CREATE PROCEDURE procActualizarDuracionVisita(
     IN v_visita_id INT,
-    IN v_duracion TIME
+    IN v_duracion VARCHAR(8) -- Cambiar de TIME a VARCHAR
 )
 BEGIN
-    UPDATE tbl_visitas
-    SET vis_duracion = v_duracion
+    -- Conversión explícita a TIME
+    UPDATE tbl_visitas 
+    SET vis_duracion = CAST(v_duracion AS TIME)
     WHERE vis_id = v_visita_id;
 END //
 DELIMITER ;
