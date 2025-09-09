3d5af94fb2456d545d69f4134528e1d9bf438c88
DanielaCharcopa <charcopadaniela178@gmail.com>
2025-09-08 20:19:19 -0500

    Se suben cambios de la tablas de la Base de datos

--- Script_tablas.sql
+++ Script_tablas.sql
@@ -126,10 +126,11 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- Table `bibliotecavm-db`.`tbl_material_edu_has_tbl_autores`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `bibliotecavm-db`.`tbl_material_edu_has_tbl_autores` (
+  `id_material_autores` INT NOT NULL AUTO_INCREMENT,
   `tbl_material_edu_mat_id` INT NOT NULL,
   `tbl_autores_au_id` INT NOT NULL,
-  `Descripcion` TEXT CHARACTER SET 'armscii8' COLLATE 'armscii8_general_ci' NOT NULL,
-  PRIMARY KEY (`tbl_material_edu_mat_id`, `tbl_autores_au_id`),
+  `Descripcion` TEXT CHARACTER SET 'armscii8' NOT NULL,
+  PRIMARY KEY (`id_material_autores`, `tbl_material_edu_mat_id`, `tbl_autores_au_id`),
   INDEX `fk_tbl_material_edu_has_tbl_autores_tbl_autores1_idx` (`tbl_autores_au_id` ASC) ,
   INDEX `fk_tbl_material_edu_has_tbl_autores_tbl_material_edu_idx` (`tbl_material_edu_mat_id` ASC) ,
   CONSTRAINT `fk_tbl_material_edu_has_tbl_autores_tbl_autores1`
